6fc5e02a0e4800e9cf9a7fcffc42559f46cbcffe
mukeshsahnis <mukeshsahnis@gmail.com>
2022-03-08 12:25:53 +0530

    Added endpoint url for /create_user/

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from api import views
+
+app_name = "api"
+
+urlpatterns = [
+    path(
+        "v1/create_customer/",
+        views.create_customer,
+        name="create_customer",
+    ),
+]
