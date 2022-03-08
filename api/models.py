0dce93f9ec69e17a2536773ea4727644c35acbfb
mukeshsahnis <mukeshsahnis@gmail.com>
2022-03-08 12:04:52 +0530

    Added customer model
    - Intially I satarted with a seperate model for customer but
      when I reach to `point-3` of part 2 I need to change my mind
      and decided make Customer model as custom user model so we
      can further user this model for user registration & authentication
    
    - Because here is stated that customer can create quote for policy
      we have to asure that the customer is register and authenticated
      before creating quote for policy.
    
    - Here authentication is must so we need to treat customer as user.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,73 @@
-from django.db import models
+import datetime as dt
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models, transaction
+
+
+class CustomerManager(BaseUserManager):
+    def create(self, email, password, first_name, last_name, dob):
+        """
+        Creates and saves a Customer with the given
+        name, email and password.
+        """
+        today = dt.datetime.now()
+        email = email
+        with transaction.atomic():
+            user = self.model(
+                email=email,
+                first_name=first_name,
+                last_name=last_name,
+                dob=dob,
+                date_joined=today,
+                is_admin=False,
+            )
+            user.set_password(password)
+            user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        email,
+        password,
+        first_name="",
+        last_name="",
+    ):
+        user = self.create(
+            email,
+            password,
+            first_name,
+            last_name,
+            dob=None,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class Customer(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    dob = models.DateField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(
+        default=dt.datetime.now,
+    )
+
+    USERNAME_FIELD = "email"
+
+    objects = CustomerManager()
+
+    def has_perm(self, *_args, **_kwargs):
+        # Is required else admin won't work
+        # Handle whether the user has a specific permission?"
+        return self.is_admin
+
+    def has_module_perms(self, *_args, **_kwargs):
+        # Is required else admin won't work
+        # Handle whether the user has permissions to view the app `app_label`?"
+        return self.is_admin
